0f0ab813aa99edf1ed2aea63e7b9f94a7c9eb526
Erik Peldan <erik.peldan@gmail.com>
2017-04-25 23:04:28 +0200

    Support finding dependencies

--- analysis.pl
+++ analysis.pl
@@ -1,9 +1,7 @@
 :- module(analysis, []).
 
-class_package(Parts, Package) :-
-  known_package(Package),
-  known_class(Class),
-  append(Package, [Class], Parts).
+class_package(QualifiedClass, Package) :-
+  append(Package, [_ClassName], QualifiedClass).
 
 %% A java class depends on another one if:
 
@@ -13,31 +11,40 @@ depends_on(JavaClass, Dependee) :-
 
 %% .. or if the classes share a package
 depends_on(JavaClass, Dependee) :-
+  Dependee \= JavaClass,
+  known_class(Dependee),
+  known_class(JavaClass),
   class_package(JavaClass, Package),
   class_package(Dependee, Package).
 
 
 % .. or if it depends on another class that depends on Dependee
 depends_on(JavaClass, Dependee) :-
-  OtherClass \= JavaClass,
-  OtherClass \= Dependee,
-  depends_on(OtherClass, Dependee),
-  depends_on(JavaClass, OtherClass).
-
-
-
-dependees_of(JavaClass, Dependees) :-
-  findall(Dependee, depends_on(JavaClass, Dependee), Dependees).
+  known_class(JavaClass),
+  imports(JavaClass, OtherClass),
+  depends_on(OtherClass, Dependee).
 
 
 imports([pkg1, classA], [pkg1, classB]).
 imports([pkg1, classB], [pkg1, classC]).
 imports([pkg1, classC], [pkg2, classD]).
+imports([pkg1, classC], [pkg2, classE]).
 
-known_package([pkg2]).
-known_package([pkg1]).
-known_class(classA).
-known_class(classB).
-known_class(classC).
-known_class(classD).
 
+known_class([pkg1, classA]).
+known_class([pkg1, classB]).
+known_class([pkg1, classC]).
+known_class([pkg2, classE]).
+
+
+% An unknown class is a class that we know about,
+% e.g because someone imported it,
+% but that hasn't itself been declared 'known'
+unknown_class(Class) :-
+  imports(_, Class),
+  \+ known_class(Class).
+
+
+dependees_of(JavaClass, Dependees) :-
+  findall(Dependee, depends_on(JavaClass, Dependee), Deps0),
+  list_to_set(Deps0, Dependees).
